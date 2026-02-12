f7068a43f17dd18ab2415c44764fdab940115b8a
LucusKim <146055401+LucusKim@users.noreply.github.com>
2026-02-11 18:11:31 -0600

    testing debug

--- lab3.py
+++ lab3.py
@@ -26,20 +26,30 @@ print('ADC Voltage: ' + str(chan0.voltage) + 'V')
 while True:
     #sample the voltages per second, 1000x a second
     samples = []
+    start = time.perf_counter()
     for _ in range(1000):
         samples.append(chan0.voltage)
         time.sleep(1/1000)
+    end = time.perf_counter()
+    total_time = end - start
     
+    #small smoothing (3-point average) to reduce noise
+    smoothing = []
+    smoothing.append(samples[0])
+    for i in range(1, len(samples) - 1):
+        smoothing.append((samples[i-1] + samples[i] + samples[i+1]) / 3)
+    smoothing.append(samples[-1])
+    
+
     #find slope of samples
     slopes = []
     for i in range(len(samples) - 1):
         slopes.append(samples[i+1]-samples[i])
-    
+
     #find average slopes
     sum_slopes = 0
     for k in slopes:
         sum_slopes += abs(k)
-
     avg_slope = sum_slopes / len(slopes)
 
     #define type
@@ -49,33 +59,32 @@ while True:
     minVoltage = min(samples)
     maxVoltage = max(samples)
     height = maxVoltage - minVoltage
-    middle = height / 2
+    middle = (minVoltage + maxVoltage) / 2
 
+    if height < 0.05:
+        print("type is ", "")
+        print("frequency is ", 0)
+        continue
     
-    
-    #check if square
+    #find flat regions
     count_flat_region = 0
-    for j in samples:
-        if(abs(j-minVoltage) < 0.25 or abs (j - maxVoltage) < 0.25):
+    for j in smoothing:
+        if(abs(j-minVoltage) < 0.08*height or abs (j - maxVoltage) < 0.08*height):
             count_flat_region += 1
     
-    if (count_flat_region > 0.75 * len(samples)):
+    if count_flat_region > 0.8 * len(smoothing):
         type = "square"
-
-    #check if triangle
-    if ((avg_slope > 0.05) and (count_flat_region < 0.75 * len(samples))):
+    elif avg_slope > 0.1 * height:
         type = "triangle"
-    
-    #check if sin
-    if (minVoltage != maxVoltage and (count_flat_region < 0.75 * len(samples)) and (avg_slope > 0.05)):
+    else:
         type = "sine"
 
     #determine frequency, where counting number times cross midline in second
     frequency = 0
-    for m in range(1,len(samples)):
-        if(samples[m-1] < middle and samples[m] > middle):
+    for m in range(1,len(smoothing)):
+        if(smoothing[m-1] < middle and smoothing[m] > middle):
             frequency += 1
-    
+    frequency = frequency / total_time
 
     #print result
     print("type is ", type)
